4a5bca6b00e9d59c4486a48d47e6d47b3eb40edf
Ppunpprem <151167611+Ppunpprem@users.noreply.github.com>
2025-03-06 14:40:39 +0700

    change floor no to string

--- G2 - Copy (2)/backend/models/Bin.js
+++ G2 - Copy (2)/backend/models/Bin.js
@@ -16,7 +16,7 @@ const binSchema = new mongoose.Schema({
   bin_location: { type: Object, required: true },
   bin_name_location: { type: String, required: true },
   bin_info_correction: { type: Number, default: 0 },
-  bin_floor_number: { type: Number, required: true },
+  bin_floor_number: { type: String, required: true },
   bin_features_general_waste: { type: Boolean, required: true },
   bin_features_recycle_waste: { type: Boolean, required: true },
   bin_features_organic_waste: { type: Boolean, required: true },
